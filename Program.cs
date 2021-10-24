081fe8aeb01bf402114bfe8d503ee06852779731
mohamedoselim <imokhamedusamaelsai@urfu.me>
2021-10-25 03:44:37 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ohms.law_hopooe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I value  :");
+            int I = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("R value  :");
+            int R = Convert.ToInt32(Console.ReadLine());
+            int result = I * R;
+            Console.WriteLine(" V = " + result);
+
+        }
+    } 
+}
